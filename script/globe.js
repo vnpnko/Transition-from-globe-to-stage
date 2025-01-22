81f27722241d8451e76d3f7c10e71cc47007fa01
Ivan Pylypenko <vn.pnko@gmail.com>
2025-01-23 00:13:31 +0100

    Clouds, geotags, lines between locations are added

--- script/globe.js
+++ script/globe.js
@@ -1,17 +1,78 @@
-const labelsTopOrientation = new Set(['Mauritania']);
+import * as THREE from 'https://esm.sh/three';
 
-const moon = new Globe(document.getElementById('globe'))
-    .globeImageUrl('assets/moon/earth_surface.jpg')
+const arcsData = [
+    {
+        startLat: 21.2339,
+        startLng: -11.0653,   // Mauritania
+        endLat: 37.5903,
+        endLng: 23.4341       // Athens
+    },
+    {
+        startLat: 47.2954,
+        startLng: 19.0207,    // Budapest
+        endLat: 37.5903,
+        endLng: 23.4341       // Athens
+    }
+];
+
+const marker =
+    `
+        <div style="transform: translate(-50%, -50%) translateX(-15px) translateY(10px) ">
+          <img src="assets/globe/geotag.png" alt="Geotag" style="width:50px; height:50px" />
+        </div>
+    `;
+
+const globe = new Globe(document.getElementById('globe'))
+    .globeImageUrl('assets/globe/earth_surface_old.jpg')
+    .backgroundImageUrl("assets/globe/night-sky.png")
+
+    // labels
     .labelText('label')
-    .showAtmosphere(false)
-    .backgroundImageUrl("assets/moon/night-sky.png")
     .labelSize(2)
-    .labelDotRadius(0.4)
-    .labelDotOrientation(d => labelsTopOrientation.has(d.label) ? 'top' : 'bottom')
-    .labelColor(() => "white")
-    .onLabelClick(d => window.open(d.url, '_self'));
+    .labelColor(() => "red")
+    .labelDotOrientation('top')
+    .onLabelClick(d => window.open(d.url, '_self'))
 
-fetch('data/locations.json').then(r => r.json()).then(landingSites => {
-    moon.labelsData(landingSites);
+    // arcs
+    .arcsData(arcsData)
+    .arcColor(() => "yellow")
+    .arcStroke(1)
+    .arcAltitude(0)
+
+    // geotags
+    .htmlElement(d => {
+        const el = document.createElement('div');
+        el.innerHTML = marker;
+        el.style.width = '10px';
+        el.style['pointer-events'] = 'auto';
+        el.style.cursor = 'pointer';
+        el.onclick = () => window.open(d.url, '_self');
+        return el;
+    })
+;
+
+
+const CLOUDS_ALT = 0.004;
+const CLOUDS_ROTATION_SPEED = -0.006;
+
+new THREE.TextureLoader().load('assets/globe/clouds_compressed.png', cloudsTexture => {
+    const clouds = new THREE.Mesh(
+        new THREE.SphereGeometry(globe.getGlobeRadius() * (1 + CLOUDS_ALT), 75, 75),
+        new THREE.MeshPhongMaterial({ map: cloudsTexture, transparent: true })
+    );
+    globe.scene().add(clouds);
+
+    (function rotateClouds() {
+        clouds.rotation.y += CLOUDS_ROTATION_SPEED * Math.PI / 180;
+        requestAnimationFrame(rotateClouds);
+    })();
+});
+
+fetch('data/locations.json')
+    .then(r => r.json())
+    .then(landingSites => {
+    globe
+        .labelsData(landingSites)
+        .htmlElementsData(landingSites);
 });
 
